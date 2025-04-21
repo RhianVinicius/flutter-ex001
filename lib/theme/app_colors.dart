4408a481d447815533663feb5a9512652c8a9edb
Rhian Vinícius <rianresende497@gmail.com>
2025-04-21 13:26:03 -0300

    create file for the app colors palette

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFFF55423);
+  static const primaryLight = Color(0xFFFF7636);
+  static const primaryDark = Color(0xFFE03C1F);
+
+  static const background =  Color(0xFFFFF0D5);
+
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF1B1B1D);
+}
